a372c14173573e44f5725d54890922eca4634a94
[Norah Taylor] <emt368@gmail.com>
2022-07-18 17:14:48 -0400

    video background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import './App.css'
 //useState takes the initial state 
 
 function App() {
@@ -8,16 +9,22 @@ function App() {
   //const [count, setCount] = useState(0)
   //left, variable state itsself, on right is the function that updates the variable on the left 
 
-  
-  return (
-    <div >
 
-      {/* <h1>Hello React</h1>
-      <h2>count is {count}</h2>
-      <button onClick={() => { setCount(count +1) } }>CLICK ME</button> */}
+  return (
+    <div className='screen' >
+      <video autoPlay muted loop id="myVideo">
+        <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mp4
+        "/>
+      </video>
+      <div className="" id="fs-overlay">
+      <img id="header-img" src="https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png" />
+      </div>
     </div>
   );
 }
 
 export default App;
 
+{/* <h1>Hello React</h1>
+      <h2>count is {count}</h2>
+      <button onClick={() => { setCount(count +1) } }>CLICK ME</button> */}
